de211c57e316db9176ba01c2e46c408ed8c85be9
chondosha1234 <jmiller61193@gmail.com>
2023-04-26 00:09:36 -0400

    added my Theme composable, and changes actionbar, also removed appcompat and unnecessary themes and colors

--- app/src/main/java/com/chondosha/codapizza/MainActivity.kt
+++ app/src/main/java/com/chondosha/codapizza/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.chondosha.codapizza
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.activity.compose.setContent
-import com.chondosha.codapizza.model.Topping
-import com.chondosha.codapizza.model.ToppingPlacement
-import com.chondosha.codapizza.ui.PizzaBuilderScreen
-import com.chondosha.codapizza.ui.ToppingCell
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.chondosha.codapizza.ui.AppTheme
+import com.chondosha.codapizza.ui.PizzaBuilderScreen
+
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PizzaBuilderScreen()
+            AppTheme {
+                PizzaBuilderScreen()
+            }
         }
     }
 
